a2c8475bbf7afe39f2a7944292b890b40575a361
thegksingh <gksingh1106@gmai.com>
2025-10-18 10:11:18 +0530

    add count_words function

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ def open_file(filename):
     with open(f"{filename}.txt","r") as file:
         return(file.read())
     
- 
+def count_words(s):
+    words = s.split()
+    print(len(words))
 
 if __name__ == "__main__":
     main()
