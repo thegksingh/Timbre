07f31bab231b9c3919fc2d053660535fa47dd2c5
thegksingh <gksingh1106@gmai.com>
2025-10-18 10:36:07 +0530

    create type_effect and apply to greet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pyfiglet
 import shutil
-
+import time
 
 def main():
     ...
@@ -8,7 +8,7 @@ def main():
 def greet(s):
     columns = shutil.get_terminal_size().columns
     art = pyfiglet.figlet_format(s, width=columns, justify="center")
-    print(art)
+    type_effect(art,0.03)
 
 def open_file(filename):
     with open(f"{filename}.txt","r") as file:
@@ -17,6 +17,11 @@ def open_file(filename):
 def count_words(s):
     words = s.split()
     print(len(words))
+
+def type_effect(s, delay):
+    for character in s:
+        print(character, end="", flush=True)
+        time.sleep(delay)
 
 if __name__ == "__main__":
     main()
